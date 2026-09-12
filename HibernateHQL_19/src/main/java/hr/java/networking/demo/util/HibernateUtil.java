fae9aa45f59042d6a1663116257063c1a96b810e
ejelacic073 <72506886+ejelacic07@users.noreply.github.com>
2026-09-12 16:50:10 +0200

    Add files via upload

---
+++ HibernateHQL_19/src/main/java/hr/java/networking/demo/util/HibernateUtil.java
@@ -0,0 +1,24 @@
+package hr.java.networking.demo.util;
+
+import lombok.Getter;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    @Getter
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
+
