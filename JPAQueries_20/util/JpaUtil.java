50ca5278961dbbf5f77ad44f8a38711076067d04
ejelacic073 <72506886+ejelacic07@users.noreply.github.com>
2026-09-12 17:03:58 +0200

    Add files via upload

---
+++ JPAQueries_20/util/JpaUtil.java
@@ -0,0 +1,22 @@
+package hr.java.jpa.demo.util;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaUtil {
+    private static final EntityManagerFactory emFactory;
+
+    static {
+        emFactory = Persistence.createEntityManagerFactory("books");
+    }
+
+    public static EntityManager getEntityManager() {
+        return emFactory.createEntityManager();
+    }
+
+    public static void close() {
+        emFactory.close();
+    }
+}
